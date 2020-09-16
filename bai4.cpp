3d454beb6c0ffa02f99a7a40f1cc58fcf7fb8df3
Ho Quang Linh <hoquanglinh2710@gmail.com>
2020-09-16 11:15:38 +0700

    Some descriptive message for the commit

--- bai4.cpp
+++ bai4.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-	cout<<"Hello world";
-	cout<<"Hao ";
+	cout<<"what's";
+	cout<<"e nhoc";
 }
---
+++ greeting.cs
@@ -0,0 +1,11 @@
+using System;
+namespace greeting
+{
+    class Greeting
+    {
+        void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
